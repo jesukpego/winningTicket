1264108852e85611244604142c5c3ba8e98094b1
jesukpego <awenoudejesukegotheophane@gmail.com>
2026-01-12 23:11:03 +0000

    Fix: Update to stable package versions for Railway

--- config/settings.py
+++ config/settings.py
@@ -114,7 +114,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
 
 # Media files
 MEDIA_URL = '/media/'
